ba99772f1afcdb596cbcde9988c542c38c77bbc4
zubair-pc\zubair <szubair@gmail.com>
2021-04-27 16:10:19 +0500

    Basic set up of Spring Boot Appplication,Just added spring web depency in this project through spring initilizer

---
+++ src/main/kotlin/com/example/SpringBootSwagger2Use/SpringBootSwagger2UseApplication.kt
@@ -0,0 +1,11 @@
+package com.example.SpringBootSwagger2Use
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootSwagger2UseApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBootSwagger2UseApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/SpringBootSwagger2Use/SpringBootSwagger2UseApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.SpringBootSwagger2Use
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootSwagger2UseApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
